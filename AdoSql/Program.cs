6ebeac065156dd79dda442e503b82f3e41241553
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-27 19:34:41 +0800

    Add project files.

---
+++ AdoSql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdoSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
