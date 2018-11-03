0722d2e0e18733e9a1d3cb8feec206dbbd42c62f
Julius Bacosa <juliusbacosa@gmail.com>
2018-11-03 20:40:38 +0800

    MVC

---
+++ ConsoleAppx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppx
+{
+    public class Planet
+    {
+        private string _name, _address;
+        public Planet(string Name, string Address)
+        {
+            _name = Name;
+            _address = Address;
+        }
+        public string GetName() => _name;
+        public string GetAddress() => _address;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planet = new Planet("Earth", "3rd Ring");
+            Console.WriteLine($"{planet.GetName()} : {planet.GetAddress()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EFCOREMVC/WEBMVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBMVC.Models;
+
+namespace WEBMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+        public HomeController(DataContext ctx) => context = ctx;
+
+        public IActionResult Index() => View();
+
+        public IActionResult Respond() => View();
+
+    }
+}
