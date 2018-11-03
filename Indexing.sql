51c0d13c34561b5d26ec8622d8339a7618fd4683
Julius Bacosa <juliusbacosa@gmail.com>
2018-11-03 11:37:38 +0800

    indexing

---
+++ Indexing.sql
@@ -0,0 +1,11 @@
+﻿/*
+	CREATE INDEX <indexName> ON <tableName> (<columnList>) ON <ﬁlegroup>;
+*/
+
+/*
+
+	CREATE TABLE <tableName> (  
+	…  <primaryKeyColumn> int CONSTRAINT PKTableName ON <ﬁleGroup>  … 
+	);
+
+*/
