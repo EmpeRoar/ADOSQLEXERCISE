8e011a0e1c3aaa9c9b998bad6e871d2cb494c59c
Julius Bacosa <juliusbacosa@gmail.com>
2018-11-03 21:13:31 +0800

    EF core and MVC

--- EFCOREMVC/WEBMVC/Controllers/HomeController.cs
+++ EFCOREMVC/WEBMVC/Controllers/HomeController.cs
@@ -16,5 +16,19 @@ namespace WEBMVC.Controllers
 
         public IActionResult Respond() => View();
 
+        [HttpPost] public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks), 
+                new {
+                    Name = response.Name,
+                    WillAttend = response.WillAttend
+                });
+        }
+
+        public IActionResult Thanks(GuestResponse response) => View(response);
+        public IActionResult ListResponses() => View(context.Responses.OrderByDescending(r => r.WillAttend));
+
     }
 }
