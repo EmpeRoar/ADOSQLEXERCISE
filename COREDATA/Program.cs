4f78820d30d2d317aee11627ec116bb6be28b50e
Julius Bacosa <juliusbacosa@gmail.com>
2018-11-03 22:40:57 +0800

    Core Data

---
+++ COREDATA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COREDATA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
