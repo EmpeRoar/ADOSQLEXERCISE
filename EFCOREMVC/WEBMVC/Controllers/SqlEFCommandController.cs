095cc23bc7123f7a459679c57c868c5f3010366d
Julius Bacosa <juliusbacosa@gmail.com>
2018-11-03 22:12:58 +0800

    EF execute SqlCommands

---
+++ EFCOREMVC/WEBMVC/Controllers/SqlEFCommandController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBMVC.Models;
+
+namespace WEBMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlEFCommandController : ControllerBase
+    {
+        private DataContext context;
+        public SqlEFCommandController(DataContext ctx) => context = ctx;
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Go()
+        {
+            context.Database.ExecuteSqlCommand($@"Insert into Responses(Name) values ('julius')");
+            return "Go!";
+        }
+    }
+}
