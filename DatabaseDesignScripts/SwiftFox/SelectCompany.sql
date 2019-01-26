159633de2a6c50ba91d227bfb32daf40e6de7f2f
Julius Bacosa <julius@innoventsoftware.com.au>
2019-01-27 00:57:39 +0800

    creating swiftfox db

---
+++ DatabaseDesignScripts/SwiftFox/SelectCompany.sql
@@ -0,0 +1,15 @@
+SELECT C.OrganizationName, 
+       C.RegisteredName, 
+	   GA.AddressLine1, 
+	   GA.AddressLine2, 
+	   GC.Value, 
+	   GLA.Value as AddressType
+FROM [Organization].[Company] AS C
+  INNER JOIN [Organization].[CompanyAddress] AS CA
+     ON C.Id = CA.CompanyId
+  INNER JOIN [General].[Address] AS GA
+     ON GA.Id = CA.AddressId
+  INNER JOIN [GeneralLookup].[Country] AS GC
+     ON GC.Id = GA.CountryId
+  INNER JOIN [GeneralLookup].[AddressType] AS GLA
+     ON GLA.Id = GA.AddressTypeId
