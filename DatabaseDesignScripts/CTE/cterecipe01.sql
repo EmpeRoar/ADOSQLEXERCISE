8b038362e5e5cac6a4ec396b8c7cf59decf875b2
Julius Bacosa <julius@innoventsoftware.com.au>
2019-01-27 16:12:20 +0800

    multiple cte

---
+++ DatabaseDesignScripts/CTE/cterecipe01.sql
@@ -0,0 +1,13 @@
+WITH	GetNamesCTE	(BusinessEntityID, FirstName, MiddleName, LastName)
+AS 
+(
+    	SELECT	BusinessEntityID, FirstName, MiddleName, LastName 
+        FROM Person.Person 
+) 
+SELECT	
+    BusinessEntityID, 
+    FirstName, 
+    MiddleName,	
+    LastName		
+FROM	
+GetNamesCTE; 
